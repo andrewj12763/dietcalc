853e7e315a0c433faf8d42968b53c4b96d1262e0
Andrew Johnson <32281250+andrewj12763@users.noreply.github.com>
2018-09-12 19:43:19 -0600

    fixing links

--- app/assets/js/cats.js
+++ app/assets/js/cats.js
@@ -2,14 +2,16 @@ console.log("Js file is working");
 // This is for the resourse button on the home page
 $("#pageonebuttonresources").on("click", function() {
   window.location.href = "resources";
+	event.preventDefault();
 });
 // This is the resource appender
 
 var title = ["title one", "title two", "title three"];
-var Link = ["www.youtube.com", "link two", "link three"];
+var Link = ['https://www.youtube.com/', "link two", "link three"];
+
 
 for (var i = 0; i < title.length; i++) {
-  var eachrow = "<div class='row datadesign'><h5> " + title[i] + "     <a href='" + Link[i] + "'>View Source Page</a>" + "</h5></div>";
+  var eachrow = "<div class='row datadesign'><h5> " + title[i] + "     <a target='_blank' rel='noopener noreferrer' href='" + Link[i] + "'>View Source Page</a>" + "</h5></div>";
 
   function addresources() {
 		$("#resourcelogic").append(eachrow);
